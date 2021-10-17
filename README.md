d212f6913b38d233b59800a30d84a653349bbb37
LucasMuratore <lucaascanch14@gmail.com>
2021-10-16 22:00:49 -0300

    Agregar archivos de proyecto.

---
+++ README.md
@@ -0,0 +1,15 @@
+# ApuestasOficial
+Desarrollo del Proyecto: Pagina de Apuestas Online sobre partidos de futbol.
+## Construido con 🛠️
+* [Visual Studio 2019.](https://visualstudio.microsoft.com)
+## Autores ✒️
+* **Valentino Indovino Bustos** - *Trabajo Inicial* - [Valen Indovino](https://github.com/valenIndovino)
+* **Juan Ignacio Otturi** - *Trabajo Inicial* - [Juan Otturi](https://github.com/juanotturi)
+* **Lucas Nahuel Muratore** - *Trabajo Inicial* - [Lucas Muratore](https://github.com/LucasMuratore)
+* **Juan Fernando Quiroz Vera** - *Trabajo Inicial* - [Fernando Quiroz](https://github.com/fernando0704)
+## Versionado 📌
+Usamos [Git](https://git-scm.com) para el versionado. Para todas las versiones disponibles, mira los [tags en este repositorio](https://github.com/tu/proyecto/tags).
+## Expresiones de Gratitud 🎁
+* Comenta a otros sobre este proyecto 📢
+* Invita una cerveza 🍺 o un café ☕ a alguien del equipo. 
+* Da las gracias públicamente 🤓.
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+using Apuestas.BaseDeDatos;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apuestas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(ConfiguracionCookie);
+
+            services.AddControllersWithViews();
+            services.AddDbContext<ApuestasDbBrowser>(options => options.UseSqlite(@"filename=C:\Users\ferdi\OneDrive\Escritorio\Apuestas.db"));
+
+        }
+
+        public static void ConfiguracionCookie(CookieAuthenticationOptions opciones)
+        {
+            opciones.LoginPath = "/Login/Login";
+            opciones.AccessDeniedPath = "/Login/NoAutorizado";
+            opciones.LogoutPath = "/Login/Logout";
+            opciones.ExpireTimeSpan = System.TimeSpan.FromMinutes(1);
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
